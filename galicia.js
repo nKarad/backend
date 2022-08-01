af7d1ba64a3bd70d914a4ee16366220790756cc1
nKarad <88811121+nKarad@users.noreply.github.com>
2022-08-01 14:42:54 -0300

    Add files via upload
    Subida

--- galicia.js
+++ galicia.js
@@ -1,24 +1,83 @@
-let cuentaPesos = 0.00
+// let cuentaPesos = 0.00
 
-function deposito(monto) {
-    cuentaPesos = cuentaPesos+monto;
+// function deposito(monto) {
+//     cuentaPesos = cuentaPesos+monto;
     
+// }
+
+// function extraccion(monto) {
+//     cuentaPesos = cuentaPesos-monto;
+    
+// }
+
+// //Test función
+
+// console.log ("su saldo es", cuentaPesos)
+
+// deposito (1000.00)
+
+// console.log ("Hola popui. Su nuevo saldo es", cuentaPesos
+// )
+
+// extraccion (400.00)
+// console.log ("Realizo una extracción. Su nuevo saldo es", cuentaPesos
+// )
+
+
+// Anon
+// const saludo = function (nombre) {
+//     console.log ("Hola", nombre)
+// }
+// q2
+// saludo ("RICARDOVICH")
+
+// Variable
+
+// let foo = function ( ) {console.log ("Ligma")};
+// foo();
+
+// Autollamada
+// (function (){
+//     console.log ("What is ligma?")
+// }) ();
+
+
+// function armarSanguche (ingredientePrincipal) {
+//     return function (complementos){
+//         console.log("El sanguche es de", (ingredientePrincipal),"con complemento adicional de" ,(complementos))
+//    }
+// }
+// const sangucheCarne = armarSanguche ("Res");
+// sangucheCarne ("JyQ")
+
+// const sanguchePollo = armarSanguche ("Pollito");
+// sanguchePollo ("tomate")
+
+function mostrarLista (lista) {
+    if (lista.leinght === 0) {
+        console.log ("lista vacia")
+    } else {
+        console.log (lista)
+    }
 }
 
-function extraccion(monto) {
-    cuentaPesos = cuentaPesos-monto;
-    
+mostrarLista ([1, 2, 3])
+mostrarLista ([]);
+
+(function (lista) {
+    if (lista.leinght === 0) {console.log ("lista vacia")
 }
+    else {console.log (lista)
+    }
 
-//Test función
+}
+    )
 
-console.log ("su saldo es", cuentaPesos)
+    ([4,5,6])
 
-deposito (1000.00)
+;
 
-console.log ("su nuevo saldo es", cuentaPesos
-)
 
-extraccion (400.00)
-console.log ("su nuevo saldo es", cuentaPesos
-)
+function crearMultiplicador (num1)
+{return}
+const cuadruplicar = 
